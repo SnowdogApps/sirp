57c46907456744df0a369cb0f426898a69a2413f
Radek <radek.zaworski@snow.dog>
2018-08-30 20:08:25 +0200

    Add error handler

--- application/effortless/controllers.py
+++ application/effortless/controllers.py
@@ -1,9 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_restful import Api
 
 from .resources.recommendation import RecommendationResource
+from .exceptions import InvalidUsage
 
 effortless = Blueprint('effortless', __name__)
+
+
+@effortless.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 
 api = Api(effortless)
 
