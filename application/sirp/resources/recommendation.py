fe40a9c4fe768c3ca8504b6b3e2424453899a15e
Radek <radek.zaworski@snow.dog>
2018-09-07 14:15:57 +0200

    Add exceptions handling during opening recommendation data

--- application/sirp/resources/recommendation.py
+++ application/sirp/resources/recommendation.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import pickle
 from flask_restful import Resource, reqparse
@@ -6,27 +7,42 @@ from application import app, logger
 from ..exceptions import InvalidUsage
 
 logger.info('Unpickling tree...')
-with open(app.config['TREE_PATH'], 'rb') as f:
-    tree = pickle.load(f)
-logger.info('Tree unpickled')
+try:
+    with open(app.config['TREE_PATH'], 'rb') as f:
+        tree = pickle.load(f)
+        logger.info('Tree unpickled')
+except IOError:
+    logger.info('Could not read tree file')
+    sys.exit()
 
 logger.info('Unpickling features...')
-with open(app.config['FEATURES_PATH'], 'rb') as f:
-    features = pickle.load(f)
-logger.info('Features unpickled')
+try:
+    with open(app.config['FEATURES_PATH'], 'rb') as f:
+        features = pickle.load(f)
+        logger.info('Features unpickled')
+except IOError:
+    logger.info('Could not read features file')
+    sys.exit()
 
 logger.info('Loading image list...')
-with open(app.config['IMG_CSV_PATH'], 'r') as f:
-    reader = csv.reader(f)
-    ids = [row[0] for row in reader]
-logger.info('Image list loaded')
+try:
+    with open(app.config['IMG_CSV_PATH'], 'r') as f:
+        reader = csv.reader(f)
+        ids = [row[0] for row in reader]
+        logger.info('Image list loaded')
+except IOError:
+    logger.info('Could not read image list')
+    sys.exit()
+
 
 
 class RecommendationResource(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('number', required=False, type=int,
+        self.parser.add_argument('limit', required=False, type=int,
                                  location='args', default=5)
+        self.parser.add_argument('without_first', required=False, type=bool,
+                                 location='args', default=True)
 
     def get(self, id):
         args = self.parser.parse_args()
@@ -38,7 +54,7 @@ class RecommendationResource(Resource):
 
         dists, recommendation_idxs = tree.query(
             [features[idx]],
-            k=args['number'],
+            k=args['limit'],
             sort_results=True
         )
 
